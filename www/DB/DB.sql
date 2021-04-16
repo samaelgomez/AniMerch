069db6b9042e6f6a8f1c779e120b61aa3e4f06aa
samaelgomez <samaelgomezherrera@gmail.com>
2021-04-16 16:37:09 +0200

    DB update

--- www/DB/DB.sql
+++ www/DB/DB.sql
@@ -23,6 +23,7 @@ CREATE TABLE if not exists figures (
     brand VARCHAR(200),
     franchise VARCHAR(200),
     visits INT,
+    stock INT,
     likes INT DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY (brand) REFERENCES shops(shopName)
@@ -33,13 +34,13 @@ CREATE TABLE if not exists images (
 	path VARCHAR(200)
 );
 
-INSERT INTO figures (figureName, type, price, image, brand, franchise, visits)
-VALUES ("Zoro", "Standard", "39", "view/images/Zoro.jpg", "Kadokawa", "OnePiece", 9),
-	   ("Madara", "Statue", "999", "view/images/Madara.png", "GoodSmile", "Naruto", 14),
-       ("2B", "Nendoroid", "59", "view/images/2B.png", "Alter", "Nier", 8),
-       ("Miku", "Standard", "44", "view/images/Miku.png", "Aniplex", "Vocaloid", 11),
-	   ("Kirishima", "Nendoroid", "69", "view/images/Kirishima.png", "Kadokawa", "BNHA", 5),
-	   ("Ichigo", "Statue", "1399", "view/images/Ichigo.png", "GoodSmile", "Bleach", 22);
+INSERT INTO figures (figureName, type, price, image, brand, franchise, visits, stock)
+VALUES ("Zoro", "Standard", "39", "view/images/Zoro.jpg", "Kadokawa", "OnePiece", 9, 24),
+	   ("Madara", "Statue", "999", "view/images/Madara.png", "GoodSmile", "Naruto", 14, 8),
+       ("2B", "Nendoroid", "59", "view/images/2B.png", "Alter", "Nier", 8, 19),
+       ("Miku", "Standard", "44", "view/images/Miku.png", "Aniplex", "Vocaloid", 11, 36),
+	   ("Kirishima", "Nendoroid", "69", "view/images/Kirishima.png", "Kadokawa", "BNHA", 5, 14),
+	   ("Ichigo", "Statue", "1399", "view/images/Ichigo.png", "GoodSmile", "Bleach", 22, 6);
        
 INSERT INTO images (name, path)
 VALUES ("Banner1", "view/images/Banner1.png"),
